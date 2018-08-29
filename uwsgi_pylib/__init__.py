edc500c897971eb6ab994a108eda6bffeebd3018
Peter Baumgartner <pete@lincolnloop.com>
2018-08-29 16:29:05 -0600

    Fix for py2.7

--- uwsgi_pylib/__init__.py
+++ uwsgi_pylib/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 UWSGI_VERSION = "2.0.17.1"
-UWSGI_LIB = os.path.join(
-    os.path.dirname(__file__), "libuwsgi-{}.so".format(UWSGI_VERSION)
+UWSGI_LIB = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "libuwsgi-{}.so".format(UWSGI_VERSION))
 )
